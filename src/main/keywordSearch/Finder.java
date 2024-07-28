f11b8652fd84e19e49d7ebd57e0773b2d2f02088
abhinandan.g <abhinandan.g@media.net>
2024-07-28 14:33:22 +0530

    initialized finder class

--- src/main/keywordSearch/Finder.java
+++ src/main/keywordSearch/Finder.java
@@ -1,5 +1,7 @@
 package keywordSearch;
 
+import utils.Pair;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -8,17 +10,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Finder implements Runnable {
-    private final ConcurrentLinkedQueue<String> tokens;
+    private final ConcurrentLinkedQueue<Pair<Integer, String>> tokens;
     public final Object monitor;
     private final String filePath;
     private final ConcurrentHashMap<Integer, String> resultMap;
-    private final Integer qId;
 
-    Finder(ConcurrentLinkedQueue<String> tokens, Object monitor, String filePath, Integer qId, ConcurrentHashMap<Integer, String> resultMap) {
+    Finder(ConcurrentLinkedQueue<Pair<Integer, String>> tokens, Object monitor, String filePath, ConcurrentHashMap<Integer, String> resultMap) {
         this.tokens = tokens;
         this.monitor = monitor;
         this.filePath = filePath;
-        this.qId = qId;
         this.resultMap = resultMap;
     }
 
@@ -26,8 +26,8 @@ public class Finder implements Runnable {
     public void run() {
         while(true) {
             try{
-                String tokenToSearch = tokens.poll();
-                if(tokenToSearch == null) {
+                Pair<Integer, String> query = tokens.poll();
+                if(query == null) {
                     synchronized(this.monitor) {
                             this.monitor.wait();
                             continue;
@@ -40,13 +40,13 @@ public class Finder implements Runnable {
                 while((line=br.readLine())!=null)
                 {
                     String[] lineArr = line.split(":", 1);
-                    if(lineArr[0].equalsIgnoreCase(tokenToSearch)) {
-                        resultMap.put(qId, lineArr[1]);
+                    if(lineArr[0].equalsIgnoreCase(query.getSecond())) {
+                        resultMap.put(query.getFirst(), lineArr[1]);
                         break;
                     }
                 }
-                if(!resultMap.containsKey(qId))
-                    resultMap.put(qId, "Not Found");
+                if(!resultMap.containsKey(query.getFirst()))
+                    resultMap.put(query.getFirst(), "Not Found");
             } catch(InterruptedException | IOException e) {
                 e.printStackTrace();
             }
