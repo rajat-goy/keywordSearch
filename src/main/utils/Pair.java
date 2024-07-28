f724a2d3d3a90a3297850889025d0e6d24ebcea8
Rajat <rajat.goy@media.net>
2024-07-28 12:05:07 +0530

    added Pair utils

---
+++ src/main/utils/Pair.java
@@ -0,0 +1,52 @@
+package utils;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Pair<F, S> implements Serializable {
+    private final F first;
+    private final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Pair(Pair<F,S> pair) {
+        this.first = pair.first;
+        this.second = pair.second;
+    }
+
+    public static <F, S> Pair<F, S> of(F first, S second) {
+        return new Pair<>(first, second);
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair pair = (Pair) o;
+        return first.equals(pair.getFirst()) && second.equals(pair.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
